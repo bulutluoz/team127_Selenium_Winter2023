7c4981bcff83ce0e216fcd75951a7bbdbf9701dd
bulutluoz <abbulutluoz@gmail.com>
2023-11-15 12:47:08 +0100

    3.gun sonu

---
+++ src/test/java/day04_xpath/C01_Xpath.java
@@ -0,0 +1,14 @@
+package day04_xpath;
+
+public class C01_Xpath {
+
+    public static void main(String[] args) {
+
+
+        //1- https://testotomasyonu.com/addremove/ adresine gidin
+        //2- Add Element butonuna basin
+        //3- Delete butonu’nun gorunur oldugunu test edin
+        //4- Delete tusuna basin
+        //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+    }
+}
